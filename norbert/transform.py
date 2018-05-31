1de8a2bf588bc87c7e5403f1e2d4583f9e93e2af
Fabian-Robert Stöter <fabian-robert.stoeter@audiolabs-erlangen.de>
2018-05-31 20:28:16 +0200

    add unit tests for transform module

--- norbert/transform.py
+++ norbert/transform.py
@@ -4,19 +4,20 @@ from scipy.signal import stft, istft
 
 class TF(object):
     """Time-Frequency transformation aka. spectrogram"""
-    def __init__(self, fs=44100, n_fft=2048, overlap=1024):
+    def __init__(self, fs=44100, n_fft=2048, overlap=None):
         self.fs = fs
         self.n_fft = n_fft
         self.overlap = overlap
+        self.input_shape = None
 
     def transform(self, x):
-        if x.ndim > 1:
-            x = np.mean(x, axis=1)
-        f, t, X = stft(x, nperseg=self.n_fft, noverlap=self.overlap)
+        self.input_shape = x.shape
+        f, t, X = stft(x.T, nperseg=self.n_fft, noverlap=self.overlap)
         X = np.flipud(X)
         return X
 
     def inverse_transform(self, X):
         X = np.flipud(X)
         t, audio = istft(X, self.fs, noverlap=self.overlap)
-        return audio
+        audio = audio[:, :self.input_shape[0]]
+        return audio.T
---
+++ tests/test_transform.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pytest
+import norbert.transform as transform
+
+
+@pytest.fixture(params=[1024, 2048])
+def nb_win(request):
+    return request.param
+
+
+@pytest.fixture(params=[None])
+def nb_hop(request):
+    return request.param
+
+
+@pytest.fixture(params=[1, 2])
+def nb_channels(request):
+    return request.param
+
+
+@pytest.fixture(params=[1, 2])
+def nb_samples(request, rate):
+    return int(request.param * rate)
+
+
+@pytest.fixture(params=[8000, 22050, 44100])
+def rate(request):
+    return request.param
+
+
+@pytest.fixture
+def x(request, nb_samples, nb_channels):
+    return np.random.random((nb_samples, nb_channels))
+
+
+def test_reconstruction(x, rate, nb_win, nb_hop):
+    tf = transform.TF(fs=rate, n_fft=nb_win, overlap=nb_hop)
+
+    X = tf.transform(x)
+    y = tf.inverse_transform(X)
+
+    assert np.allclose(x, y)
