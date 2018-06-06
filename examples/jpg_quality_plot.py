1a0c05d341b23e6ecd761017d493e5087aa75140
Fabian-Robert Stöter <fabian-robert.stoeter@audiolabs-erlangen.de>
2018-06-06 14:00:03 +0200

    fancy plotting

--- examples/jpg_quality_plot.py
+++ examples/jpg_quality_plot.py
@@ -12,14 +12,25 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     df = pd.read_pickle(args.pickle)
-    print(df)
-    g = sns.factorplot(
-        x="quality",
-        y="ODG",
-        data=df,
-        legend_out=False,
-        dodge=False,
-    )
+    df['size'] = df['size'].astype(int)
+    df['bitrate'] = df['size'] / 7.0
+    df['quality'] = df['quality'].astype(int)
+    df['ODG'] = df['ODG'].astype(int)
+    agg = df.groupby(['quality'], as_index=False).mean()
+    min_quality = min(agg[agg['ODG'] > -1]['quality'])
 
-    plt.legend(loc='upper left', title="JPG Quality (Mono)")
+with plt.xkcd():
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax = agg.plot(x="quality", y="ODG", kind="line", ax=ax, color='b', legend=False)
+    ax2 = ax.twinx()
+    agg.plot(x="quality", y="bitrate", kind="line", ax=ax2, color="r", legend=False)
+    ax.set_ylabel("PEAQ ODG Score", color='b')
+    ax2.set_ylabel("Bitrate in bps", color='r')
+    ax.set_xlabel("JPEG Quality Parameter")
+    ax.axvspan(min_quality, 100, facecolor='#2ca02c', alpha=0.2)
+    ax.tick_params('y', colors='b')
+    ax2.tick_params('y', colors='r')
+    plt.title("Reconstruction Error (Mono)")
     plt.savefig("mono.png")
+    plt.show()
