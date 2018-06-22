88d66cb7a05d6c4f0dc40e298a45db462f3f1fa2
Fabian-Robert Stöter <fabian-robert.stoeter@audiolabs-erlangen.de>
2018-06-22 16:19:46 +0200

    add oracle separation

--- examples/oracle_separation.py
+++ examples/oracle_separation.py
@@ -15,19 +15,25 @@ def oracle(track, separation_fn):
     x = tf.transform(track. audio)
 
     # prepare the spectrograms of the sources
-    max = ls.scale(bw.downsample(np.sum(np.abs(x)**2, axis=-1))).max()
+    # get maximum of mixture
+    ls.scale(bw.downsample(np.sum(np.abs(x)**2, axis=-1)))
+    bounds = None
+    print(bounds)
+    # bounds = None
     v = []
     for name, value in track.sources.items():
         v_j = np.sum(np.abs(tf.transform(value.audio))**2,
                      axis=-1, keepdims=True)
 
-        v_j_Q = qt.quantize(ls.scale(bw.downsample(v_j), max))
+        v_j_Q = qt.quantize(ls.scale(bw.downsample(v_j), bounds))
 
         v_j_bar = bw.upsample(
-                        ls.unscale(
-                            qt.dequantize(v_j_Q),
-                            max)
-                        )
+            ls.unscale(
+                qt.dequantize(v_j_Q),
+                bounds
+            )
+        )
+
         v += [np.squeeze(v_j_bar)]
 
     v = np.moveaxis(np.array(v), 0, 2)
@@ -45,10 +51,10 @@ if __name__ == '__main__':
     mus = musdb.DB()
     mus.run(
         functools.partial(
-            oracle, separation_fn=norbert.wiener
+            oracle, separation_fn=norbert.softmask
         ),
         estimates_dir='test_wiener',
         subsets='test',
-        parallel=True,
-        cpus=2
+        parallel=False,
+        cpus=1
     )
