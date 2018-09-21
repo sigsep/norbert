be23048ba1f437f0c2bac7e5ee49c4b9d9dd265d
Fabian-Robert Stöter <fabian-robert.stoeter@audiolabs-erlangen.de>
2018-09-22 00:07:30 +0200

    lets stick to 0.1.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='norbert',
-    version='0.1.1',
+    version='0.1.0',
     description='Spectrogram Models',
     long_description=long_description,
     long_description_content_type='text/markdown',
