35a55df8f8c0cdcb9b352f8f1d972d2d50359d3b
Fabian-Robert Stöter <fabian-robert.stoeter@audiolabs-erlangen.de>
2018-09-26 21:03:02 +0200

    lets wait for travis 3.7 support

--- setup.py
+++ setup.py
@@ -28,8 +28,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.6'
     ],
 
     keywords='wiener filter',
