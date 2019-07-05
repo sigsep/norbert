fbfe7ff10e106d681c3063d3cb2a9d02d244dd9c
Fabian-Robert Stöter <fabian-robert.stoter@inria.fr>
2019-07-05 14:37:02 +0200

    remove support for 3.5

--- setup.py
+++ setup.py
@@ -27,7 +27,6 @@ setup(
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7'
     ],
